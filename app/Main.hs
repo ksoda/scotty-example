a83d3f5d4de76a442d07261bef074620c796939f
Ken Sonoda <ken.soda0@gmail.com>
2016-11-18 01:01:35 +0900

    Hello

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
-
-import Lib
+import Web.Scotty
+import Data.Monoid (mconcat)
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Starting Server..."
+  scotty 3000 $
+    get "/hello" $
+      text "hello world!"
+-- main = scotty 3000 $ do
+--   get "/:word" $ do
+--     beam <- param "word"
+--     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
