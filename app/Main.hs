ea3bfa6154b6c4264e4f6f3c0933a6e614ddfc73
Ken Sonoda <ken.soda0@gmail.com>
2016-11-23 02:02:27 +0900

    Login /plain password

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 module Main where
-import GHC.Generics
 
 import Web.Spock
 import Web.Spock.Config
 import Network.HTTP.Types.Status (status400)
+import Network.Wai.Handler.Warp (Port)
+import System.Environment (getEnvironment)
 import Control.Monad.IO.Class (MonadIO)
-import qualified Data.Text as Text
--- import Data.Aeson (FromJSON, ToJSON)
+import Data.List (lookup)
+import Data.Maybe
+import qualified Data.Text as T
 
--- data User = User { userName :: String
---                  , password :: String
---                  } deriving (Show, Generic)
-
--- instance ToJSON User
--- instance FromJSON User
-
-userList :: [(Text, Text)]
+userList :: [(T.Text, T.Text)]
 userList =
   [ ("lotz", "password1")
   , ("alice", "password2")
   , ("bob", "password3")
   ]
-  -- [ User { userName = "lotz", password = "password1"}
-  -- , User { userName = "alice", password = "password2"}
-  -- , User { userName = "bob", password = "password3"}
-  -- ]
 
-findCredential :: MonadIO m => ActionCtxT ctx m (Maybe (Text, Text))
+findCredential :: MonadIO m => ActionCtxT ctx m (Maybe (T.Text, T.Text))
 findCredential = do
   username <- param "username"
   password <- param "password"
@@ -37,14 +27,17 @@ findCredential = do
 
 main :: IO ()
 main = do
+  port <- getPort
+  putStr "start Server: http://localhost:"
+  print port
   spockCfg <- defaultSpockCfg () PCNoDatabase ()
-  runSpock 8080 $ spock spockCfg $ do
+  runSpock port $ spock spockCfg $ do
 
     get root $
       text "Hello Spock!"
 
     get ("hello" <//> var) $ \name ->
-      text (Text.concat ["Hello ", name, "!"])
+      text (T.concat ["Hello ", name, "!"])
 
     post "login" $ do
       credential <- findCredential
@@ -54,3 +47,11 @@ main = do
           if lookup username userList == Just password
             then text "Login succeed."
             else setStatus status400 >> text "Wrong parameter."
+
+getPort :: IO Port
+getPort = fmap port getEnvironment
+  where
+    port = maybe defaultPort read . lookup "PORT"
+
+defaultPort :: Port
+defaultPort = 3000
